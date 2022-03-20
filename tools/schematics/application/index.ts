b2dae9aac8a3f4e946836af52ab62b9a3c0248fa
Jack Vincent <50340752+allocadia-jack@users.noreply.github.com>
2022-03-19 17:00:25 -0700

    build: fix eslint errors before checking whole solution in ci

--- tools/schematics/application/index.ts
+++ tools/schematics/application/index.ts
@@ -1,16 +1,16 @@
 import {
   Rule,
-  SchematicContext,
   Tree,
   chain,
   externalSchematic,
 } from "@angular-devkit/schematics";
+import { Schema as ApplicationOptions } from "@schematics/angular/application/schema";
 
 // Follows example at https://blog.angular.io/schematics-an-introduction-dc1dfbc2a2b2
-export function application(options: any): Rule {
+export function application(options: ApplicationOptions): Rule {
   return chain([
     externalSchematic("@schematics/angular", "application", options),
-    (tree: Tree, _context: SchematicContext) => {
+    (tree: Tree) => {
       return tree;
     },
   ]);
