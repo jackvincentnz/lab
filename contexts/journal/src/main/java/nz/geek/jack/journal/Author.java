1e933e140f0b152d29852744aa4934ce8adb4d35
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2023-07-21 16:03:15 -0700

    feat(journal): add author as example of child data fetcher

---
+++ contexts/journal/src/main/java/nz/geek/jack/journal/Author.java
@@ -0,0 +1,21 @@
+package nz.geek.jack.journal;
+
+public final class Author {
+
+  private final String id;
+
+  private final String name;
+
+  public Author(String id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
