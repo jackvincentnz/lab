036448ab080598d7509a436b876b013a629d03dd
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2023-08-01 16:56:54 -0700

    feat(task): switch domain to use eventing entity

---
+++ contexts/task/src/main/java/nz/geek/jack/task/domain/TaskCompletedEvent.java
@@ -0,0 +1,8 @@
+package nz.geek.jack.task.domain;
+
+import nz.geek.jack.libs.domain.DomainEvent;
+
+public final class TaskCompletedEvent extends DomainEvent {
+
+  public TaskCompletedEvent() {}
+}
---
+++ libs/domain/src/main/java/nz/geek/jack/libs/domain/Aggregate.java
@@ -0,0 +1,95 @@
+package nz.geek.jack.libs.domain;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Aggregate {
+
+  private static final String REDUCER_METHOD_NAME = "on";
+
+  private static final Map<String, Method> REDUCER_METHODS = new HashMap<>();
+
+  private final List<DomainEvent> appliedEvents = new ArrayList<>();
+
+  protected void apply(DomainEvent domainEvent) {
+    appliedEvents.add(domainEvent);
+    reduce(domainEvent);
+  }
+
+  private void reduce(DomainEvent domainEvent) {
+    var eventType = domainEvent.getClass();
+
+    var reducer = getReducerFor(eventType);
+
+    try {
+      reducer.invoke(this, domainEvent);
+    } catch (InvocationTargetException e) {
+      var reductionFailedMessage =
+          String.format(
+              "Method %s(%s) failed. See cause: %s",
+              REDUCER_METHOD_NAME, eventType.getSimpleName(), e.getMessage());
+
+      if (e.getCause() != null) {
+        throw new RuntimeException(reductionFailedMessage, e.getCause());
+      }
+
+      throw new RuntimeException(reductionFailedMessage, e);
+
+    } catch (IllegalAccessException e) {
+      var reductionFailedMessage =
+          String.format(
+              "Method %s(%s) failed because of illegal access. See cause: %s",
+              REDUCER_METHOD_NAME, eventType.getSimpleName(), e.getMessage());
+
+      throw new RuntimeException(reductionFailedMessage, e);
+    }
+  }
+
+  private Method getReducerFor(Class<? extends DomainEvent> eventType) {
+    var thisType = this.getClass();
+
+    var methodKey = String.format("%s:%s", thisType.getName(), eventType.getName());
+
+    var reducer = REDUCER_METHODS.get(methodKey);
+    if (reducer == null) {
+      reducer = getAndCacheReducerFor(methodKey, eventType);
+    }
+
+    return reducer;
+  }
+
+  private Method getAndCacheReducerFor(String methodKey, Class<? extends DomainEvent> eventType) {
+    synchronized (REDUCER_METHODS) {
+      var method = findReducer(eventType);
+      method.setAccessible(true);
+
+      REDUCER_METHODS.put(methodKey, method);
+
+      return method;
+    }
+  }
+
+  private Method findReducer(Class<? extends DomainEvent> eventType) {
+    try {
+      return this.getClass().getDeclaredMethod(REDUCER_METHOD_NAME, eventType);
+    } catch (Exception e) {
+      var reducerNotFoundMessage =
+          String.format(
+              "Reducer %s(%s) not found: %s >>> %s",
+              REDUCER_METHOD_NAME,
+              eventType.getSimpleName(),
+              e.getClass().getSimpleName(),
+              e.getMessage());
+
+      throw new IllegalArgumentException(reducerNotFoundMessage, e);
+    }
+  }
+
+  public List<DomainEvent> getAppliedEvents() {
+    return appliedEvents;
+  }
+}
