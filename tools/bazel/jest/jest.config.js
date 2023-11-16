276bbcb5ae58306d4d6e30f46d386cce77465ee6
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2023-11-15 17:10:31 -0800

    build: jest optimization to avoid wasted transform effort

--- tools/bazel/jest/jest.config.js
+++ tools/bazel/jest/jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.css$": "jest-transform-stub",
-    "screenfull/index.js$": "@swc/jest",
   },
-  transformIgnorePatterns: ["/node_modules/(?!(screenfull/index.js))/"],
 };
