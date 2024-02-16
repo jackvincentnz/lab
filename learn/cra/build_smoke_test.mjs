ec3a996ca95cb270216b43dbd77239cf1e4b0448
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-02-16 15:14:06 -0800

    chore: remove old cra example

--- learn/cra/build_smoke_test.mjs
+++
@@ -1,7 +0,0 @@
-import assert from "assert";
-import fs from "fs";
-
-// Make sure there's a file like build/static/js/main.12345678.js
-const files = fs.readdirSync("learn/cra/build/static/js");
-console.log(files);
-assert.ok(files.some((f) => /main\.[0-9a-f]{8}\.js/.test(f)));
