af064d0a36c77b8ea78d2b3c78189d1ed3630fb9
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-02-16 16:36:56 -0800

    feat(bubbles): expose library types to consumers

--- apps/journal_app/src/App.tsx
+++ apps/journal_app/src/App.tsx
@@ -1,11 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { MantineProvider } from "@mantine/core";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { Shell } from "@lab/bubbles"; // FIXME: type safety for the library
+import { Shell } from "@lab/bubbles";
 import { EntriesPage } from "./entries";
-
-import "@lab/bubbles/style.css";
 
 const client = new ApolloClient({
   uri: "/graphql",
--- apps/tasklist/src/app/App.tsx
+++ apps/tasklist/src/app/App.tsx
@@ -1,10 +1,6 @@
 import { MantineProvider } from "@mantine/core";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { Shell } from "@lab/bubbles"; // FIXME: type safety for the library
+import { Shell } from "@lab/bubbles";
 import Tasks from "../tasks";
-
-import "@lab/bubbles/style.css";
 
 export default function App() {
   return (
