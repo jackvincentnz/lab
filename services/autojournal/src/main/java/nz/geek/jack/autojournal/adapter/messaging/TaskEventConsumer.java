eb34c1f741246ef58c83726dee2e291b36d9a695
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-02-23 16:04:14 -0800

    fix(autojournal): stop listening if handler encounters error

--- services/autojournal/src/main/java/nz/geek/jack/autojournal/adapter/messaging/TaskEventConsumer.java
+++ services/autojournal/src/main/java/nz/geek/jack/autojournal/adapter/messaging/TaskEventConsumer.java
@@ -1,23 +1,24 @@
 package nz.geek.jack.autojournal.adapter.messaging;
 
 import com.google.protobuf.Message;
+import java.util.List;
 import nz.geek.jack.autojournal.application.TaskEventHandler;
+import nz.geek.jack.task.api.TaskAddedEvent;
 import nz.geek.jack.task.api.TaskCompletedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
+@KafkaListener(id = "autojournal", topics = "task")
 public class TaskEventConsumer {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TaskEventConsumer.class);
 
-  private static final String LISTENER_ID = "autojournal";
-
-  private static final String TASK_TOPIC = "task";
+  private static final List<Class<? extends Message>> IGNORED_MESSAGES =
+      List.of(TaskAddedEvent.class);
 
   private final TaskEventHandler taskEventHandler;
 
@@ -25,18 +26,34 @@ public class TaskEventConsumer {
     this.taskEventHandler = taskEventHandler;
   }
 
-  // TODO: Transactional consume
-  @KafkaListener(id = LISTENER_ID, topics = TASK_TOPIC)
-  public void listen(Message message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-    LOGGER.info(
-        String.format(
-            "Consumed event from topic %s: value = %s", topic, message.getClass().getName()));
+  @KafkaHandler
+  public void on(TaskCompletedEvent taskCompletedEvent) {
+    LOGGER.info(String.format("Handling [%s]", taskCompletedEvent.getClass().getName()));
 
-    // TODO: move out to task event handler factory or use wrapper message with defined set of
-    // events
-    if (message instanceof TaskCompletedEvent m) {
-      taskEventHandler.handle(
-          new nz.geek.jack.autojournal.application.TaskCompletedEvent(m.getTaskId()));
+    taskEventHandler.handle(
+        new nz.geek.jack.autojournal.application.TaskCompletedEvent(
+            taskCompletedEvent.getTaskId()));
+  }
+
+  @KafkaHandler(isDefault = true)
+  public void on(Message message) {
+    /*
+     * This is an example of making sure we aren't failing to observe and update some state from a new message that an
+     * upstream producer is newly sending. It is typically a bad idea to ignore unknown messages if it means we will be
+     * eventually inconsistent with the upstream state.
+     *
+     * An alternative is having no default handler and an empty handler for each ignored message. That would leave
+     * Spring to throw when it can't find a matching handler for a known message.
+     *
+     * This needs to be used in conjunction with a CommonContainerStoppingErrorHandler so that the container stops
+     * listening on error. E.g. we usually assume we need to fix something in this case before we start listening again.
+     */
+    if (IGNORED_MESSAGES.contains(message.getClass())) {
+      LOGGER.debug(String.format("Ignoring [%s]", message.getClass().getName()));
+    } else {
+      throw new RuntimeException(
+          String.format(
+              "Received unknown [%s] which hasn't been ignored", message.getClass().getName()));
     }
   }
 }
--- services/task/src/test/java/nz/geek/jack/task/TaskApplicationTest.java
+++ services/task/src/test/java/nz/geek/jack/task/TaskApplicationTest.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
-@EmbeddedKafka(
-    partitions = 1,
-    brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka
 public class TaskApplicationTest {
 
   @Test
