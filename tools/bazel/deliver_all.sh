e80174e200abba375cba791e4ee83b50bfd291c4
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-03-27 16:23:10 -0700

    docs: add xargs parallel comment

--- tools/bazel/deliver_all.sh
+++ tools/bazel/deliver_all.sh
@@ -7,4 +7,5 @@ oci_push_targets=$(bazel query 'kind("oci_push", //...)' --output label)
 oci_push_targets_count=$(echo "$oci_push_targets" | wc -l)
 
 # Parallel run all oci_push targets in release mode (with version stamping enabled)
+# xargs drops stdout entries in parallel mode. Remove -P if debugging. See: https://stackoverflow.com/a/32463179
 echo "$oci_push_targets" | xargs -P "$oci_push_targets_count" -n 1 bazel run --config=release
--- tools/bazel/deliver_changed.sh
+++ tools/bazel/deliver_changed.sh
@@ -7,4 +7,5 @@ targets=$(bazel query 'kind("push_if_not_exists", //...)' --output label)
 targets_count=$(echo "$targets" | wc -l)
 
 # Parallel run all delivery targets in release mode (with version stamping enabled)
+# xargs drops stdout entries in parallel mode. Remove -P if debugging. See: https://stackoverflow.com/a/32463179
 echo "$targets" | xargs -P "$targets_count" -n 1 bazel run --config=release
