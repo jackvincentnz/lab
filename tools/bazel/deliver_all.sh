219cfaf09bdf1c22688986cd2bbf6bef5596f446
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-03-27 13:34:54 -0700

    build: push in parallel

--- tools/bazel/deliver_all.sh
+++ tools/bazel/deliver_all.sh
@@ -3,5 +3,8 @@
 # Find all executable oci_push targets
 oci_push_targets=$(bazel query 'kind("oci_push", //...)' --output label)
 
-# Run all oci_push targets in release mode (with version stamping enabled)
-echo "$oci_push_targets" | xargs -n 1 bazel run --config=release
+# Learn target count for parallel pushes
+oci_push_targets_count=$(echo "$oci_push_targets" | wc -l)
+
+# Parallel run all oci_push targets in release mode (with version stamping enabled)
+echo "$oci_push_targets" | xargs -P "$oci_push_targets_count" -n 1 bazel run --config=release
