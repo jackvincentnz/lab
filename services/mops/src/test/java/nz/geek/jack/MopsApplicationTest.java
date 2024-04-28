1f3da3d5287a5ebd911ce9b0c89acc34a7820a42
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-04-27 20:45:37 -0700

    feat(mops): setup application with tenant api

---
+++ services/mops/src/test/java/nz/geek/jack/MopsApplicationTest.java
@@ -0,0 +1,11 @@
+package nz.geek.jack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MopsApplicationTest {
+
+  @Test
+  public void contextLoads() {}
+}
