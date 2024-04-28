6295ff3c872be19a96fe00fd2b1bfd8984e64517
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-04-27 20:58:37 -0700

    refactor(mops): nest iam into mops

---
+++ services/mops/src/test/java/nz/geek/jack/mops/iam/domain/identity/TenantIdTest.java
@@ -0,0 +1,24 @@
+package nz.geek.jack.mops.iam.domain.identity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class TenantIdTest {
+
+  @Test
+  void create_setsId() {
+    var id = TenantId.create();
+
+    assertThat(id).isNotNull();
+  }
+
+  @Test
+  void fromString_setsId() {
+    var idString = TenantId.create().toString();
+
+    var id = TenantId.fromString(idString);
+
+    assertThat(id.toString()).isEqualTo(idString);
+  }
+}
