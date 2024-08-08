ebfb39c0a48a8e0c0084ed29188fe6c4b88d5ce7
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-08-08 15:35:23 -0700

    feat(mops_app): add basic app shell

--- apps/mops_app/src/App.tsx
+++ apps/mops_app/src/App.tsx
@@ -1,7 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { Shell } from "./components/shell";
 
 export default function App() {
-  return <MantineProvider theme={theme}>Hello world!</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Shell />
+    </MantineProvider>
+  );
 }
---
+++ apps/mops_app/src/components/shell/Navbar.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Group, Code, Text } from "@mantine/core";
+import {
+  IconBellRinging,
+  IconCalendarEvent,
+  IconFingerprint,
+  IconKey,
+  IconSettings,
+  Icon2fa,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+} from "@tabler/icons-react";
+import classes from "./Navbar.module.css";
+
+const data = [
+  { link: "", label: "Activities", icon: IconCalendarEvent },
+  { link: "", label: "Notifications", icon: IconBellRinging },
+  { link: "", label: "Billing", icon: IconReceipt2 },
+  { link: "", label: "Security", icon: IconFingerprint },
+  { link: "", label: "SSH Keys", icon: IconKey },
+  { link: "", label: "Databases", icon: IconDatabaseImport },
+  { link: "", label: "Authentication", icon: Icon2fa },
+  { link: "", label: "Other Settings", icon: IconSettings },
+];
+
+export function Navbar() {
+  const [active, setActive] = useState("Billing");
+
+  const links = data.map((item) => (
+    <a
+      className={classes["link"]}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes["linkIcon"]} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes["navbar"]}>
+      <div className={classes["navbarMain"]}>
+        <Group className={classes["header"]} justify="space-between">
+          <Text>Mops</Text>
+          <Code fw={700}>v3.1.2</Code>
+        </Group>
+        {links}
+      </div>
+
+      <div className={classes["footer"]}>
+        <a
+          href="#"
+          className={classes["link"]}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconSwitchHorizontal className={classes["linkIcon"]} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a
+          href="#"
+          className={classes["link"]}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconLogout className={classes["linkIcon"]} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
