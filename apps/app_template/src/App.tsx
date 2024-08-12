33e5c7893464723d935c03a3c78afa4fe6dd6f09
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2024-08-12 10:18:52 -0700

    feat(app_template): align to mantine vite starter

--- apps/app_template/src/App.tsx
+++ apps/app_template/src/App.tsx
@@ -1,7 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { Router } from "./Router";
 
 export default function App() {
-  return <MantineProvider theme={theme}>Hello world!</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
+  );
 }
