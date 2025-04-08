b716aed5bdcd642e3ca5d7af8d285b9053ca6c3b
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2025-04-08 08:58:49 -0700

    feat(mops): add budget domain

---
+++ services/mops/src/main/java/nz/geek/jack/mops/core/domain/budget/BudgetCreatedEvent.java
@@ -0,0 +1,3 @@
+package nz.geek.jack.mops.core.domain.budget;
+
+public record BudgetCreatedEvent(BudgetId id, String name) {}
---
+++ services/mops/src/main/java/nz/geek/jack/mops/core/domain/budget/BudgetRepository.java
@@ -0,0 +1,7 @@
+package nz.geek.jack.mops.core.domain.budget;
+
+import nz.geek.jack.libs.ddd.domain.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BudgetRepository extends BaseRepository<Budget, BudgetId> {}
