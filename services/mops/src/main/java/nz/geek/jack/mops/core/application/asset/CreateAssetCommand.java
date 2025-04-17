c14c5788ebd23a0a508473777c919d39c37b0b22
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2025-04-16 21:02:57 -0700

    feat(mops): add basic asset domain

---
+++ services/mops/src/main/java/nz/geek/jack/mops/core/application/asset/CreateAssetCommand.java
@@ -0,0 +1,3 @@
+package nz.geek.jack.mops.core.application.asset;
+
+public record CreateAssetCommand(String name) {}
---
+++ services/mops/src/main/java/nz/geek/jack/mops/core/domain/asset/AssetId.java
@@ -0,0 +1,23 @@
+package nz.geek.jack.mops.core.domain.asset;
+
+import java.util.UUID;
+import nz.geek.jack.libs.ddd.domain.AbstractId;
+
+public final class AssetId extends AbstractId {
+
+  private AssetId() {
+    super();
+  }
+
+  private AssetId(UUID id) {
+    super(id);
+  }
+
+  public static AssetId create() {
+    return new AssetId();
+  }
+
+  public static AssetId fromString(String id) {
+    return new AssetId(UUID.fromString(id));
+  }
+}
