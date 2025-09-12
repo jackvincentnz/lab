b759ffe573a914b66d4b989ed0a5150290302fef
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2025-09-12 14:42:09 -0700

    feat(mops): asynchronously complete new user message

--- projects/mops/mops_service/src/main/java/lab/mops/ai/application/chat/ChatEventHandler.java
+++ projects/mops/mops_service/src/main/java/lab/mops/ai/application/chat/ChatEventHandler.java
@@ -1,12 +1,15 @@
 package lab.mops.ai.application.chat;
 
+import lab.mops.ai.domain.chat.ChatMessageAddedEvent;
 import lab.mops.ai.domain.chat.ChatRepository;
 import lab.mops.ai.domain.chat.ChatStartedEvent;
+import lab.mops.ai.domain.chat.Message;
 import lab.mops.ai.domain.chat.MessageStatus;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+// FIXME: there is no guarantee these will run, so an assistant message could remain PENDING
 @Component
 public class ChatEventHandler {
 
@@ -19,7 +22,6 @@ public class ChatEventHandler {
     this.completionService = completionService;
   }
 
-  // FIXME: there is no guarantee this will run, so an assistant message could remain PENDING
   @Async
   @EventListener
   public void onChatStarted(ChatStartedEvent event) {
@@ -30,6 +32,20 @@ public class ChatEventHandler {
             chat.getMessages().stream()
                 .filter(m -> !m.getStatus().equals(MessageStatus.PENDING))
                 .toList());
+
+    chat.completeMessage(event.pendingAssistantMessageId(), response);
+
+    chatRepository.save(chat);
+  }
+
+  @Async
+  @EventListener
+  public void onChatMessageAdded(ChatMessageAddedEvent event) {
+    var chat = chatRepository.getById(event.chatId());
+
+    var response =
+        completionService.getResponse(
+            chat.getMessages().stream().filter(Message::isCompleted).toList());
 
     chat.completeMessage(event.pendingAssistantMessageId(), response);
 
