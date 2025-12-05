0b37fd8f42ec1032d9482226c345503859cc75ba
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2025-12-05 15:35:46 -0800

    feat(mops): map tool requests and responses into prompt

---
+++ projects/mops/mops_service/src/main/java/lab/mops/ai/infrastructure/llm/SpringMessageMapper.java
@@ -0,0 +1,67 @@
+package lab.mops.ai.infrastructure.llm;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lab.mops.ai.application.chat.completions.AssistantMessage;
+import lab.mops.ai.application.chat.completions.ToolCall;
+import lab.mops.ai.application.chat.completions.ToolResultMessage;
+import lab.mops.ai.application.chat.completions.UserMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.ToolResponseMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringMessageMapper {
+
+  public List<Message> map(List<lab.mops.ai.application.chat.completions.Message> messages) {
+    return messages.stream()
+        .map(
+            m ->
+                switch (m.getRole()) {
+                  case USER -> map((UserMessage) m);
+                  case ASSISTANT -> map((AssistantMessage) m);
+                  case TOOL -> map((ToolResultMessage) m);
+                })
+        .collect(Collectors.toList());
+  }
+
+  private static org.springframework.ai.chat.messages.UserMessage map(UserMessage m) {
+    return new org.springframework.ai.chat.messages.UserMessage(m.content());
+  }
+
+  private static org.springframework.ai.chat.messages.AssistantMessage map(
+      AssistantMessage message) {
+    var content = message.getContent().orElse(null);
+    var toolCalls = message.getToolCalls().stream().map(SpringMessageMapper::map).toList();
+
+    return new org.springframework.ai.chat.messages.AssistantMessage(content, Map.of(), toolCalls);
+  }
+
+  private static org.springframework.ai.chat.messages.AssistantMessage.ToolCall map(ToolCall call) {
+    return new org.springframework.ai.chat.messages.AssistantMessage.ToolCall(
+        call.id(), "function", call.toolName(), call.arguments());
+  }
+
+  private static org.springframework.ai.chat.messages.ToolResponseMessage map(
+      ToolResultMessage message) {
+    return new ToolResponseMessage(
+        List.of(
+            new ToolResponseMessage.ToolResponse(
+                message.toolCallId(), message.toolName(), message.content())));
+  }
+
+  public AssistantMessage map(ChatResponse response) {
+    return AssistantMessage.of(
+        response.getResult().getOutput().getText(),
+        response.getResult().getOutput().getToolCalls().stream()
+            .map(SpringMessageMapper::map)
+            .toList());
+  }
+
+  public static ToolCall map(
+      org.springframework.ai.chat.messages.AssistantMessage.ToolCall toolCall) {
+    return new ToolCall(toolCall.id(), toolCall.name(), toolCall.arguments());
+  }
+}
---
+++ projects/mops/mops_service/src/test/java/lab/mops/ai/infrastructure/llm/SpringMessageMapperTest.java
@@ -0,0 +1,108 @@
+package lab.mops.ai.infrastructure.llm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import lab.mops.ai.application.chat.completions.AssistantMessage;
+import lab.mops.ai.application.chat.completions.ToolCall;
+import lab.mops.ai.application.chat.completions.ToolResultMessage;
+import lab.mops.ai.application.chat.completions.UserMessage;
+import lab.mops.core.api.ai.BudgetTools;
+import lab.mops.core.application.budget.data.ResolvedBudget;
+import nz.geek.jack.test.TestBase;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.tool.annotation.Tool;
+
+class SpringMessageMapperTest extends TestBase {
+
+  SpringMessageMapper messageMapper = new SpringMessageMapper();
+
+  @Test
+  void map_mapsUserMessage() {
+    var content = randomString();
+    var userMessage = new UserMessage(content);
+
+    var result = messageMapper.map(List.of(userMessage));
+
+    var mappedMessage = (org.springframework.ai.chat.messages.UserMessage) result.get(0);
+    assertThat(mappedMessage.getText()).isEqualTo(content);
+  }
+
+  @Test
+  void map_mapsAssistantMessage() {
+    var content = randomString();
+    var toolId = randomString();
+    var toolName = randomString();
+    var toolArguments = randomString();
+    var toolCalls = new ToolCall(toolId, toolName, toolArguments);
+    var assistantMessage = AssistantMessage.of(content, List.of(toolCalls));
+
+    var result = messageMapper.map(List.of(assistantMessage));
+
+    var mappedMessage = (org.springframework.ai.chat.messages.AssistantMessage) result.get(0);
+    assertThat(mappedMessage.getText()).isEqualTo(content);
+    var mappedToolCall = mappedMessage.getToolCalls().get(0);
+    assertThat(mappedToolCall.id()).isEqualTo(toolId);
+    assertThat(mappedToolCall.name()).isEqualTo(toolName);
+    assertThat(mappedToolCall.arguments()).isEqualTo(toolArguments);
+  }
+
+  @Test
+  void map_mapsToolResultMessage() {
+    var content = randomString();
+    var toolCallId = randomString();
+    var toolName = randomString();
+    var toolResultMessage = new ToolResultMessage(content, toolCallId, toolName);
+
+    var result = messageMapper.map(List.of(toolResultMessage));
+
+    var mappedMessage = (org.springframework.ai.chat.messages.ToolResponseMessage) result.get(0);
+    var mappedToolResponse = mappedMessage.getResponses().get(0);
+    assertThat(mappedToolResponse.id()).isEqualTo(toolCallId);
+    assertThat(mappedToolResponse.name()).isEqualTo(toolName);
+    assertThat(mappedToolResponse.responseData()).isEqualTo(content);
+  }
+
+  @Test
+  void map_mapsChatResponse() {
+    var content = randomString();
+    var id = randomString();
+    var toolName = randomString();
+    var arguments = randomString();
+    var chatResponse =
+        org.springframework.ai.chat.model.ChatResponse.builder()
+            .generations(
+                List.of(
+                    new Generation(
+                        new org.springframework.ai.chat.messages.AssistantMessage(
+                            content,
+                            Map.of(),
+                            List.of(
+                                new org.springframework.ai.chat.messages.AssistantMessage.ToolCall(
+                                    id, randomString(), toolName, arguments))))))
+            .build();
+
+    var result = messageMapper.map(chatResponse);
+
+    assertThat(result.getContent()).hasValue(content);
+    var toolCall = result.getToolCalls().iterator().next();
+    assertThat(toolCall.id()).isEqualTo(id);
+    assertThat(toolCall.toolName()).isEqualTo(toolName);
+    assertThat(toolCall.arguments()).isEqualTo(arguments);
+  }
+
+  static class TestTools implements BudgetTools {
+    @Tool
+    @Override
+    public void createBudget(String name) {}
+
+    @Tool
+    @Override
+    public Collection<ResolvedBudget> getAllBudgets() {
+      return List.of();
+    }
+  }
+}
