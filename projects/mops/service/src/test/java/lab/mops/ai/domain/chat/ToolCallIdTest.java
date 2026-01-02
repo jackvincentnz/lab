c0133617450bd6e201b6197d0ebeec782a53b0e6
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2026-01-02 14:27:43 -0800

    refactor(mops): add ability to create tool id

--- projects/mops/service/src/test/java/lab/mops/ai/domain/chat/ToolCallIdTest.java
+++ projects/mops/service/src/test/java/lab/mops/ai/domain/chat/ToolCallIdTest.java
@@ -16,8 +16,15 @@ class ToolCallIdTest extends TestBase {
   }
 
   @Test
+  void create_setsId() {
+    var id = ToolCallId.create();
+
+    assertThat(id.toString()).isNotBlank();
+  }
+
+  @Test
   void equals_trueForSameId() {
-    var id1 = ToolCallId.of(randomId());
+    var id1 = ToolCallId.create();
     var id2 = ToolCallId.of(id1.toString());
 
     assertThat(id1).isEqualTo(id2);
@@ -25,15 +32,15 @@ class ToolCallIdTest extends TestBase {
 
   @Test
   void equals_falseForDifferentIds() {
-    var id1 = ToolCallId.of(randomId());
-    var id2 = ToolCallId.of(randomId());
+    var id1 = ToolCallId.create();
+    var id2 = ToolCallId.create();
 
     assertThat(id1).isNotEqualTo(id2);
   }
 
   @Test
   void hashCode_sameForEqualIds() {
-    var id = ToolCallId.of(randomId());
+    var id = ToolCallId.create();
     var another = ToolCallId.of(id.toString());
 
     assertThat(id.hashCode()).isEqualTo(another.hashCode());
