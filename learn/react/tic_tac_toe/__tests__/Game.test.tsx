b1ab694f664c00738cb8bbf729e37b9abe581b86
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2026-05-22 20:12:22 -0700

    chore(learn): remove old learning examples

--- learn/react/tic_tac_toe/__tests__/Game.test.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@jest/globals";
-import { render, screen } from "@testing-library/react";
-import Game from "../Game";
-
-test("renders Game", () => {
-  render(<Game />);
-  const goToButtonElement = screen.getByText(/Go to/i);
-  expect(goToButtonElement).toBeInTheDocument();
-});
--- learn/rules_jest/ts_simple/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { it, expect } from "@jest/globals";
-
-import index from "./index";
-
-it("should work", () => {
-  expect(index).toBe("test");
-});
