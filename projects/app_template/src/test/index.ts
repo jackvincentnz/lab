57c35dea5aa1836ff3595d383cdda73a7c952a28
Jack Vincent <50340752+jackvincentnz@users.noreply.github.com>
2026-05-22 20:10:34 -0700

    chore(app_template): remove unnecessary template

--- projects/app_template/src/test/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "vitest";
-export * from "@testing-library/react";
-export { default as userEvent } from "@testing-library/user-event";
-
-export { render } from "./render";
-export * from "./test-utils";
--- projects/app_template/src/theme.ts
+++
@@ -1,5 +0,0 @@
-import { createTheme, MantineThemeOverride } from "@mantine/core";
-
-export const theme: MantineThemeOverride = createTheme({
-  /* Put your mantine theme override here */
-});
